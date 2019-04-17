38a7b143d6f9ccce2b3f6b08f95feec64af9a081
Kostya12rus <mkg97@yandex.ru>
2019-04-17 03:32:08 +0300

    new type

--- MiniMapDraw.lua
+++ MiniMapDraw.lua
@@ -1,7 +1,8 @@
 local MiniMapDraw = {}
+local KostyaUtils = require("KostyaUtils/Utils")
 MiniMapDraw.TrigerActiv = Menu.AddOption({"Kostya12rus", "MiniMapDraw"}, "Off/On", "")
 MiniMapDraw.IconSize = Menu.AddOption({"Kostya12rus", "MiniMapDraw"}, "Size minimap icon", "", 500, 1500, 100)
-MiniMapDraw.HeroIconOnly = Menu.AddOption({"Kostya12rus", "MiniMapDraw"}, "Only Hero icon", "")
+--[[ MiniMapDraw.HeroIconOnly = Menu.AddOption({"Kostya12rus", "MiniMapDraw"}, "Only Hero icon", "") ]]
 
 function MiniMapDraw.OnParticleCreate(particle)
 	if not Menu.IsEnabled(MiniMapDraw.TrigerActiv) then return end
@@ -11,11 +12,21 @@ function MiniMapDraw.OnParticleCreate(particle)
 		end
 	end
 	if not MiniMapDraw.TableParticle[particle.index] then
+		local npc = nil
+		if NPCs.Contains(particle.entity) and not NPCs.Contains(particle.entityForModifiers) then
+			npc = particle.entity
+		end
+		if not NPCs.Contains(particle.entity) and NPCs.Contains(particle.entityForModifiers) then
+			npc = particle.entityForModifiers
+		end
+		if NPCs.Contains(particle.entity) and NPCs.Contains(particle.entityForModifiers) then
+			npc = particle.entityForModifiers
+		end
 		MiniMapDraw.TableParticle[particle.index] = 
 		{
-			entity = particle.entity,
-			poscast0 = nil,
-			poscast1 = nil,
+			name = particle.name,
+			entity = npc,
+			pos = nil,
 			timing = GameRules.GetGameTime() + 3,
 			drawing = false
 		}
@@ -25,11 +36,19 @@ end
 function MiniMapDraw.OnParticleUpdate(particle)
 	if not Menu.IsEnabled(MiniMapDraw.TrigerActiv) then return end
 	if MiniMapDraw.TableParticle[particle.index] then
-		if particle.controlPoint == 0 then
-			MiniMapDraw.TableParticle[particle.index].poscast0 = particle.position
+		if MiniMapDraw.CheckVector(particle.position) then
+			MiniMapDraw.TableParticle[particle.index].timing = GameRules.GetGameTime() + 3
+			MiniMapDraw.TableParticle[particle.index].drawing = false
+			MiniMapDraw.TableParticle[particle.index].pos = particle.position
 		end
-		if particle.controlPoint == 1 then
-			MiniMapDraw.TableParticle[particle.index].poscast1 = particle.position
+	else
+		if MiniMapDraw.CheckVector(particle.position) then
+			if particle.entity and NPCs.Contains(particle.entity) and Heroes.Contains(particle.entity) then
+				MiniMap.AddIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
+				Renderer.AddWorldIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, 32)
+			else
+				MiniMap.AddIconByName(nil, "minimap_plaincircle", particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
+			end
 		end
 	end
 end
@@ -37,58 +56,46 @@ end
 function MiniMapDraw.OnParticleUpdateEntity(particle)
 	if not Menu.IsEnabled(MiniMapDraw.TrigerActiv) then return end
 	if MiniMapDraw.TableParticle[particle.index] then
-		if not MiniMapDraw.TableParticle[particle.index].entity and particle.entity and NPCs.Contains(particle.entity) then
-			MiniMapDraw.TableParticle[particle.index].entity = particle.entity
+		if MiniMapDraw.TableParticle[particle.index].entity and not Heroes.Contains(MiniMapDraw.TableParticle[particle.index].entity) then
+			if particle.entity and Heroes.Contains(particle.entity) then
+				MiniMapDraw.TableParticle[particle.index].entity = particle.entity
+			end
 		end
-		if particle.controlPoint == 0 then
-			MiniMapDraw.TableParticle[particle.index].poscast0 = particle.position
+		if MiniMapDraw.CheckVector(particle.position) then
+			MiniMapDraw.TableParticle[particle.index].timing = GameRules.GetGameTime() + 3
+			MiniMapDraw.TableParticle[particle.index].drawing = false
+			MiniMapDraw.TableParticle[particle.index].pos = particle.position
 		end
-		if particle.controlPoint == 1 then
-			MiniMapDraw.TableParticle[particle.index].poscast1 = particle.position
+	else
+		if MiniMapDraw.CheckVector(particle.position) then
+			if particle.entity and NPCs.Contains(particle.entity) and Heroes.Contains(particle.entity) then
+				MiniMap.AddIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
+				Renderer.AddWorldIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, 32)
+			else
+				MiniMap.AddIconByName(nil, "minimap_plaincircle", particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
+			end
 		end
-	end
-end
-
-function MiniMapDraw.OnParticleDestroy(particle)
-	if not Menu.IsEnabled(MiniMapDraw.TrigerActiv) then return end
-	if MiniMapDraw.TableParticle[particle.index] then
-		MiniMapDraw.TableParticle[particle.index] = nil
 	end
 end
 
 function MiniMapDraw.OnUpdate()
 	Renderer.SetDrawColor(255,255,255,255)
 	if not Menu.IsEnabled(MiniMapDraw.TrigerActiv) then return end
-	for i,tableinfo in pairs(MiniMapDraw.TableParticle) do
-		if MiniMapDraw.TableParticle[i] then
-			if MiniMapDraw.TableParticle[i].timing <= GameRules.GetGameTime() then
-				MiniMapDraw.TableParticle[i] = nil
+	if MiniMapDraw.TableParticle then
+		for i,tableinfo in pairs(MiniMapDraw.TableParticle) do
+			if tableinfo then
+				if tableinfo.timing <= GameRules.GetGameTime() or not tableinfo.pos then
+					MiniMapDraw.TableParticle[i] = nil
+				end
 			end
-		end
-		if tableinfo.poscast0 then
-			tableinfo.poscast0 = MiniMapDraw.CheckVector(tableinfo.poscast0)
-		end
-		if tableinfo.poscast1 then
-			tableinfo.poscast1 = MiniMapDraw.CheckVector(tableinfo.poscast1)
-		end
-		if not tableinfo.drawing and (tableinfo.poscast0 or tableinfo.poscast1) then
-			if tableinfo.entity and Entity.IsHero(tableinfo.entity) then
-				if tableinfo.poscast0 then
-					MiniMap.AddIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.poscast0, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
+			if not tableinfo.drawing and tableinfo.pos then
+				if tableinfo.entity and NPCs.Contains(tableinfo.entity) and Heroes.Contains(tableinfo.entity) then
+					MiniMap.AddIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
+					Renderer.AddWorldIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, 32)
 					tableinfo.drawing = true
 				else
-					MiniMap.AddIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.poscast1, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
+					MiniMap.AddIconByName(nil, "minimap_plaincircle", tableinfo.pos, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
 					tableinfo.drawing = true
-				end
-			else
-				if not Menu.IsEnabled(MiniMapDraw.HeroIconOnly) then
-					if tableinfo.poscast0 then
-						MiniMap.AddIconByName(nil, "minimap_plaincircle", tableinfo.poscast0, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
-						tableinfo.drawing = true
-					else
-						MiniMap.AddIconByName(nil, "minimap_plaincircle", tableinfo.poscast1, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
-						tableinfo.drawing = true
-					end
 				end
 			end
 		end
@@ -96,27 +103,26 @@ function MiniMapDraw.OnUpdate()
 end
 
 function MiniMapDraw.CheckVector(vec)
-	local hasnpcinpos = NPCs.InRadius(vec,50,2,Enum.TeamType.TEAM_BOTH)
+	if not vec then return end
+	local hasnpcinpos = NPCs.InRadius(vec,10,2,Enum.TeamType.TEAM_BOTH)
+	Console.Print(tostring(vec))
 	if hasnpcinpos and #hasnpcinpos ~= 0 then
-		return nil
+		for i,j in pairs(hasnpcinpos) do
+			if NPCs.Contains(j) and NPC.GetUnitName(j) ~= "npc_dota_thinker" then
+				return false
+			end
+		end
 	end
-	local distto0vect = vec:Distance(Vector(0,0,0)):Length2D()
-	if distto0vect < 10 then
-		return nil
+	local realpos = KostyaUtils.RealPosInWorld(vec)
+	if realpos then
+		local realz = math.ceil(realpos:GetZ())
+		local checkz = math.ceil(vec:GetZ())
+		if (realz+10 >= checkz and realz-2 <= checkz) and checkz < 1000 and checkz > -300 then
+			Console.Print(tostring(vec) .. " | " .. tostring(realpos) .. " - прошло проверку")
+			return true
+		end
 	end
-	local x = vec:GetX()
-	local y = vec:GetY()
-	local z = vec:GetZ()
-	if x == y and x == z then
-		return nil
-	end
-	if x == y or y == z or z == x then
-		return nil
-	end
-	if x - math.floor(x) == 0 or y - math.floor(y) == 0 then
-		return nil
-	end
-	return vec
+	return false
 end
 
 function MiniMapDraw.init()
