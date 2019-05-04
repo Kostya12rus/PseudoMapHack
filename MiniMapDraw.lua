cfd9b0150cebf32fcca2b59272cfb4c4b7c4a8bb
Kostya12rus <mkg97@yandex.ru>
2019-05-04 15:43:57 +0300

    update

--- MiniMapDraw.lua
+++ MiniMapDraw.lua
@@ -23,6 +23,7 @@ function MiniMapDraw.OnParticleCreate(particle)
 		if NPCs.Contains(particle.entity) and NPCs.Contains(particle.entityForModifiers) then
 			npc = particle.entityForModifiers
 		end
+		if Entity.IsSameTeam(Heroes.GetLocal(), npc) then return end
 		MiniMapDraw.TableParticle[particle.index] = 
 		{
 			name = particle.name,
@@ -46,6 +47,7 @@ function MiniMapDraw.OnParticleUpdate(particle)
 	else
 		if MiniMapDraw.CheckVector(particle.position) then
 			if particle.entity and NPCs.Contains(particle.entity) and Heroes.Contains(particle.entity) then
+				if Entity.IsSameTeam(Heroes.GetLocal(), particle.entity) then return end
 				MiniMap.AddIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
 				Renderer.AddWorldIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, 32)
 			else
@@ -71,6 +73,7 @@ function MiniMapDraw.OnParticleUpdateEntity(particle)
 	else
 		if MiniMapDraw.CheckVector(particle.position) then
 			if particle.entity and NPCs.Contains(particle.entity) and Heroes.Contains(particle.entity) then
+				if Entity.IsSameTeam(Heroes.GetLocal(), particle.entity) then return end
 				MiniMap.AddIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, Menu.GetValue(MiniMapDraw.IconSize))
 				Renderer.AddWorldIcon(nil, Hero.GetIcon(particle.entity), particle.position, 255, 255, 255, 255, 1, 32)
 			else
@@ -93,8 +96,10 @@ function MiniMapDraw.OnUpdate()
 			end
 			if not tableinfo.drawing and tableinfo.pos and not tableinfo.ignore then
 				if tableinfo.entity and NPCs.Contains(tableinfo.entity) and Heroes.Contains(tableinfo.entity) then
-					MiniMap.AddIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
-					Renderer.AddWorldIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, 32)
+					if not Entity.IsSameTeam(Heroes.GetLocal(), tableinfo.entity) then
+						MiniMap.AddIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
+						Renderer.AddWorldIcon(nil, Hero.GetIcon(tableinfo.entity), tableinfo.pos, 255, 255, 255, 255, 3, 32)
+					end
 					tableinfo.drawing = true
 				else
 					MiniMap.AddIconByName(nil, "minimap_plaincircle", tableinfo.pos, 255, 255, 255, 255, 3, Menu.GetValue(MiniMapDraw.IconSize))
